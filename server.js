c6af34549e1f439ed4da53327ec5b6106ac8917f
Kallol Chakraborty <kallol.chakraborty@wipro.com>
2023-02-08 12:49:32 +0000

    addition: OAuth2ClientCredential

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const axios = require("axios");
 /** sap-cf-axios: to make calls via the destinations */
 const sapcfaxios = require("sap-cf-axios").default;
 
+
 /** runs at the backend */
 const app = express();
 /** setting the PORT & if not available will be set to 4000 */
@@ -15,6 +16,37 @@ const PORT = process.env.PORT || 4000;
  * req: request
  * res: response
  */
+const getOrderFromDest = async (req, res) => {
+  console.log("entering the GET req of Destination:--------->");
+
+  /** the destination is having OAuth2ClientCredentials authentication */
+  const axios = sapcfaxios("MaintenanceOrderCPI");
+  var authorization = req.headers.authorization;
+
+  /** POST method */
+  const response = await axios({
+    method: "POST",
+    url: "/http/STO/MaintenanceOrderInput",
+    params: {
+      "$format": "json"
+    },
+    headers: {
+      "content-type": "application/json",
+      authorization
+    },
+    /** payload */
+    data: {
+      "MaintenancePlanningPlant": "1710",
+      "MaintenanceOrderType": "ZSTO",
+      "MainWorkCenter": "RES-0200",
+    }
+  });
+
+  /** return */
+  res.send(response.data.d.results);
+};
+
+
 const getOrder = async (req, res) => {
   const axios = sapcfaxios("DST030");
   console.log("entering the GET req of MaintenanceOrder:--------->");
@@ -59,6 +91,7 @@ const getOperations = async (req, res) => {
 };
 
 /** endpoints */
+app.get("/getOrderFromDest", getOrderFromDest);
 app.get("/orders", getOrder);
 app.get("/operations", getOperations);
 app.get("/", getHomePage);
